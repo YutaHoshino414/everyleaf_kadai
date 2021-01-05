fa59c2ee6d013e9137f4d85bf60afd2f0e7611f5
Yuta Hoshino <y.hoshino25@gmail.com>
2021-01-05 20:17:57 +0900

    SystemSpec雛形/Factoryテストデータ準備

---
+++ spec/factories/tasks.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :task do
+    # 下記の内容は実際に作成するカラム名に合わせて変更してください
+    name { 'Factoryで作ったデフォルトのタイトル１' }
+    content { 'Factoryで作ったデフォルトのコンテント１' }
+  end
+
+  factory :second_task, class: Task do
+    name { 'Factoryで作ったデフォルトのタイトル２' }
+    content { 'Factoryで作ったデフォルトのコンテント２' }
+  end
+end
