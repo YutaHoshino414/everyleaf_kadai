1eb7f621d616e39386061cc2559e45b52d1f351a
Yuta Hoshino <y.hoshino25@gmail.com>
2021-01-05 12:14:05 +0900

    タスクの新規/編集機能を作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,9 +9,26 @@ class TasksController < ApplicationController
   end
   
   def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "タスクを作成しました！"
+    else
+      render :new
+    end
   end
 
   def Show
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "タスクを編集しました！"
+    else
+      render :edit
+    end
   end
 
   private
