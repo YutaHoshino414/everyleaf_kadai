5ffee085c3e9b6796c87820a8609ae597765f5f7
Yuta Hoshino <y.hoshino25@gmail.com>
2021-01-13 00:44:07 +0900

    終了期限・ステータス・検索時のテスト追記

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,15 +3,21 @@ FactoryBot.define do
     # 下記の内容は実際に作成するカラム名に合わせて変更してください
     name { 'デフォルトのタスク１' }
     content { 'デフォルトのコンテント１' }
+    deadline { '02021-01-03' }
+    status { 0 }
   end
 
   factory :second_task, class: Task do
     name { 'デフォルトのタスク２' }
     content { 'デフォルトのコンテント２' }
+    deadline { '02021-01-02' }
+    status { 1 }
   end
 
   factory :third_task, class: Task do
     name { 'デフォルトのタスク３' }
     content { 'デフォルトのコンテント３' }
+    deadline { '02021-01-01' }
+    status { 2 }
   end
 end
