d33a14357e7efe43037fb9dfcae5ae07f06348a0
Yuta Hoshino <y.hoshino25@gmail.com>
2021-01-16 08:13:10 +0900

    管理画面でユーザ一覧他crud機能実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,48 @@
 class Admin::UsersController < ApplicationController
+
+  def index
+    @users = User.all.includes(:tasks)
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path, notice: "ユーザーを登録しました"
+    else
+      render :new
+    end
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def show
+    @user = User.find(params[:id])
+    @tasks = @user.tasks
   end
 
-  def index
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user.id), notice: "ユーザーを編集しました"
+    else
+      render :edit
+    end
   end
+
+  def destroy
+    @user.destroy
+    redirect_to admin_users_path, notice: "ユーザーを削除しました"
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
   
   namespace :admin do
-    get 'users/new'
-    get 'users/edit'
-    get 'users/show'
-    get 'users/index'
+    resources :users
   end
   root 'tasks#index'
   resources :tasks
-  resources :users, only: [:new, :create, :show]
+  resources :users
   resources :sessions, only: [:new, :create, :destroy]
-  namespace :admin do
-    resources :users
-  end
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
