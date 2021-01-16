cf30028781c16651323f83df41496a7d95860ade
Yuta Hoshino <y.hoshino25@gmail.com>
2021-01-17 01:08:08 +0900

    systemspec管理画面のテスト・管理ユーザー操作検証

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -57,9 +57,9 @@ RSpec.describe 'ユーザー管理機能', type: :system do
         fill_in 'session_email', with: 'user02@test.com'
         fill_in 'session_password', with: 'password'
         click_button 'ログインする'
-        sleep 2
+        sleep 1
         click_link 'ログアウト'
-        sleep 3
+        sleep 2
         expect(page).to have_content 'ログアウトしました'
         expect(page).to have_content 'アカウントをお持ちの方は、下記よりログインする'
       end
@@ -73,7 +73,7 @@ RSpec.describe 'ユーザー管理機能', type: :system do
         fill_in 'session_password', with: 'password'
         click_button 'ログインする'
         expect(page).to have_content 'ユーザー04のページ'
-        sleep 3
+        sleep 1
         visit admin_users_path
         expect(page).to have_content 'ユーザー管理画面'
       end
@@ -84,6 +84,55 @@ RSpec.describe 'ユーザー管理機能', type: :system do
         click_button 'ログインする'
         visit admin_users_path
         expect(page).to have_content '管理者以外はアクセスできません'
+      end
+    end
+    context '管理ユーザーのCRUD操作検証' do
+      before do
+        visit new_session_path
+        fill_in 'session_email', with: 'user04admin@test.com'
+        fill_in 'session_password', with: 'password'
+        click_button 'ログインする'
+        visit admin_users_path
+      end
+      it 'ユーザーの新規登録ができる' do
+        click_link 'ユーザー登録'
+        fill_in 'user_name', with: 'ユーザー01'
+        fill_in 'user_email', with: 'user01@test.com'
+        select '付与する', from: 'user[admin]'
+        fill_in 'user_password', with: 'password'
+        fill_in 'user_password_confirmation', with: 'password'
+        click_button '登録する'
+        expect(page).to have_content 'ユーザー管理画面'
+        expect(page).to have_content 'ユーザー01'
+        expect(page).to have_content 'user01@test.com'
+        sleep 1
+      end
+      it '任意のユーザー詳細画面にアクセスできる' do
+        visit admin_user_path(@user2)
+        expect(page).to have_content 'ユーザー02'
+        expect(page).to have_content 'ユーザー情報'
+        sleep 1
+      end
+      it 'ユーザーの編集画面からユーザー編集できる' do
+        visit edit_admin_user_path(@user2)
+        fill_in 'user_name', with: 'ユーザー99'
+        fill_in 'user_email', with: 'user99@test.com'
+        click_button '編集する'
+        sleep 2
+        expect(page).to have_content 'ユーザーを編集しました'
+        expect(page).to have_content 'ユーザー99'
+        expect(page).to have_content 'user99@test.com'
+      end
+      it 'ユーザーの削除ができる' do
+        user = User.find_by(name: 'ユーザー02')
+        expect(page).to have_link '削除', href: admin_user_path(user)
+        page.accept_confirm do
+          click_link '削除', href: admin_user_path(user)
+          sleep 2
+        end
+        expect(page).to have_content 'ユーザーを削除しました'
+        expect(page).not_to have_content 'ユーザー02'
+        expect(page).not_to have_content 'user02@test.com'
       end
     end
   end
