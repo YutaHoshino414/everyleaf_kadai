86d91347f8a977032478620aa7c6fb6886714ca4
Yuta Hoshino <y.hoshino25@gmail.com>
2021-01-29 15:03:17 +0900

    シードデータへ管理者生成の記述

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#最初のユーザをseedで作成する
-name = "user01"
-email = "user01@test.com"
+# *** 管理ユーザー1人作成
+name = "admin"
+email = "admin01@example.com"
 password = "password"
+admin = "true"
 User.create!(
   name: name,
   email: email,
   password: password,
   password_confirmation: password,
+  admin: admin
 )
+
+
+#最初のユーザをseedで作成する(済み)
+# name = "user01"
+# email = "user01@test.com"
+# password = "password"
+# User.create!(
+#   name: name,
+#   email: email,
+#   password: password,
+#   password_confirmation: password,
+# )
 #2つ目のuser(ブラウザ操作にて作成)
 #name =test01
 #email = "test@test.com"
